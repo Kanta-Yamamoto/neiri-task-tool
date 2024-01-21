e1ff9a8e4226018c669ac4495fd3c4ce4f29da1a
Kanta-Yamamoto <84559990+Kanta-Yamamoto@users.noreply.github.com>
2024-01-21 15:10:32 +0900

    graphqlの機能を追加

--- src/pages/memo/index.tsx
+++ src/pages/memo/index.tsx
@@ -1,9 +1,40 @@
+import { gql, useQuery } from "@apollo/client";
 import { signOut } from "next-auth/react";
 
 const Memo = () => {
+  const BOOKS = gql`
+    query Books {
+      books {
+        title
+        author
+      }
+    }
+  `;
+
+  type BooksType = {
+    title: string;
+    author: string;
+  };
+
+  function Books() {
+    const { loading, error, data } = useQuery(BOOKS);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.books.map(({ title, author }: BooksType) => (
+      <div key={title}>
+        <p>
+          {title} by {author}
+        </p>
+      </div>
+    ));
+  }
+
   return (
     <>
       <h1>メモ一覧</h1>
+      <Books />
       <div>
         <button onClick={() => signOut()}>Sign out</button>
       </div>
